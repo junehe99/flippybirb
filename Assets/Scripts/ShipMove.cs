c2db81bb6018e3dd6eec6e169a8008f1a5fb4750
June He <junehe1999@gmail.com>
2015-12-12 18:20:09 +0000

    Connected screens properly, ship movement is now working.

---
+++ Assets/Scripts/ShipMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMove : MonoBehaviour {
+
+	public LevelManager levelmanager;
+	public float speed = 15.0f;
+	private Rigidbody2D rb;
+	public int upForce = 10;
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Debug.Log(collider);
+		Destroy(gameObject);
+		Debug.Log("SHIP DOWN");
+		levelmanager.LoadLevel("Lose");
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		//levelmanager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			//transform.position += Vector3.up * speed * Time.deltaTime;
+			rb.AddForce(Vector2.up *upForce);
+			
+		}
+	}
+}
